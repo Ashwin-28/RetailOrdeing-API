0dcb33ba487e1576a449482d233fa4c0ae4a1003
zorobin04 <nggopinath04@gmail.com>
2026-04-27 12:59:08 +0530

    product controller added

---
+++ RetailAPP-API/RetailAPP-API/Services/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using RetailAPP_API.DTOs.ProductDTOs;
+
+namespace RetailAPP_API.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductResponseDto>> GetAllProductsAsync(int? categoryId);
+        Task<ProductResponseDto?> GetProductByIdAsync(int id);
+        Task<ProductResponseDto> CreateProductAsync(CreateProductDto createDto);
+        Task<bool> UpdateProductAsync(int id, UpdateProductDto updateDto);
+        Task<bool> DeleteProductAsync(int id);
+        Task<bool> UpdateStockAsync(int id, UpdateStockDto stockDto);
+    }
+}
